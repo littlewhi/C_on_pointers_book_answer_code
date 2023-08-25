41c864e2ae838ea690bf64d7b9df5cf0f1eca62c
yhb <3157470765@qq.com>
2023-08-25 19:30:45 +0800

    new code\

--- charpter6/new_code.c
+++ charpter6/new_code.c
@@ -90,8 +90,6 @@ char *find_substr( const char *str, const char *substr )
             i = *( next + i );
         }
     }    
-   
-    
     return NULL;
 }
 
@@ -165,6 +163,168 @@ void a3( void )
     printf( "reversed string = %s\n", str );
 }
 
+/* answer4 code */
+void discard( char *start, char *end, int step )
+{
+    while( start < end )
+    {
+        *start = 1;
+        start += step;
+    }
+}
+
+/*
+ *  the content of some index in number pool if it is zero then is not discarded, otherwise it is discarded.
+ */
+void Eratosthenes_prime( int result[], int limit )
+{
+    char *num_pool = NULL, *pos_of_pool = NULL, *bottom_of_pool = NULL;
+    int len, j, step;
+
+    if( limit < 2 )
+        return;
+    result[0] = 2;
+    if( limit == 2 )
+        return;
+    
+    j = 1;                      /* the index of result array */
+    len = ( limit + 1 ) / 2 - 1;
+    num_pool = (char *) calloc( len, sizeof( char ) );
+    bottom_of_pool = num_pool + len;
+    
+    /* 剔除数字 */
+    for( pos_of_pool = num_pool; pos_of_pool < bottom_of_pool; pos_of_pool++ )
+    {
+        if( *pos_of_pool )
+            continue;
+        step = ( pos_of_pool - num_pool ) * 2 + 3;
+        *( result + j++ ) = step;
+        discard( pos_of_pool, bottom_of_pool, step );
+    }
+    free( num_pool );
+}
+
+void a4( void )
+{
+    int result[1000] = { 0 };
+    int *pos = result;
+    int limit = 11;
+#ifdef LIMIT_FOR_PRIME
+    limit = LIMIT_FOR_PRIME;
+#endif
+    if( limit > 1000 )
+    {
+        printf( "limit is too large\n" );
+        return;
+    }
+    printf( "limit =  %d\n", limit );
+    
+    Eratosthenes_prime( result, limit );
+    while( *pos )
+    {
+        printf( "%d ", *pos++ );
+    }
+    puts("");
+}
+
+
+/* answer5 code */
+/* charpter 5 answer4 function code */
+/* 
+ * set the bit on bit_number to one
+ */
+void set_bit( char bit_array[], unsigned int bit_number )
+{
+    bit_array[bit_number / 8] |= ( 1 << ( bit_number % 8 ) );
+}
+
+/*
+ * set the bit on bit_number to zero
+ */
+void clear_bit( char bit_array[], unsigned int bit_number )
+{
+    bit_array[bit_number / 8] &= ~( 1 << ( bit_number % 8 ) );
+}
+
+/*
+ *  set the bit on bit_number to value
+ */
+void assign_bit( char bit_array[], unsigned int bit_number, int value )
+{
+    if( value )
+        set_bit( bit_array, bit_number );
+    else
+        clear_bit( bit_array, bit_number );
+}
+
+/*
+ * return zero if the bit on bit_number is 0, otherwise return 1.
+ */
+int test_bit( char bit_array[], unsigned int bit_number )
+{
+    return bit_array[bit_number / 8] & ( 1 << ( bit_number % 8 ) ) ? 1 : 0;
+}
+
+/*
+ * use the bit_array
+ */
+void discard_bit( char bit_array[], int i, int len )
+{
+    int step = i * 2 + 3;
+    while( i < len )
+    {
+        set_bit( bit_array, i );
+        i += step;
+    }
+}
+
+void Eratosthenes_prime_bit( int result[], int limit )
+{
+    char *num_pool = NULL; 
+    int num_of_ch, i, j;
+    if( limit < 2 )
+        return;
+    result[0] = 2;
+    if( limit == 2 )
+        return;
+    
+    j = 1;
+    limit = ( limit + 1 ) / 2 - 1;
+    num_of_ch = limit / 8 + ( limit % 8 ? 1 : 0 );
+    num_pool = (char *) calloc( num_of_ch, sizeof( char ) );
+
+    for( i = 0; i < limit; i++ )
+    {
+        if( test_bit( num_pool, i ) )
+            continue;
+        *( result + j++) = i * 2 + 3;
+        discard_bit( num_pool, i, limit );
+    }
+    free( num_pool );
+}
+
+void a5( void )
+{
+    int result[1000] = { 0 };
+    int *pos = result;
+    int limit = 11;
+#ifdef LIMIT_FOR_PRIME
+    limit = LIMIT_FOR_PRIME;
+#endif 
+    if( limit > 1000 )
+    {
+        printf( "limit is too large\n" );
+        return;
+    }
+    printf( "limit =  %d\n", limit );
+    
+    Eratosthenes_prime_bit( result, limit );
+    while( *pos )
+    {
+        printf( "%d ", *pos++ );
+    }
+    puts("");
+}
 
 /* entry main */
 int main( int argc, char **argv )
@@ -187,6 +347,21 @@ int main( int argc, char **argv )
             break;
         case '3':
             a3();
+            break;
+#if 0
+    4 and 5，I make all index is link to an odd number rather than 
+all normal numbers being larger than 2. 
+    The link between index and odd number is as follows.
+    index 0 -> 3, index 1 -> 5,
+    index 2 -> 7, index 3 -> 9, ...
+    so : 
+    index i -> (i * 2 + 3)
+#endif
+        case '4':
+            a4();
+            break;
+        case '5':
+            a5();
             break;
         default:
             puts( "Unkonwn option" );
