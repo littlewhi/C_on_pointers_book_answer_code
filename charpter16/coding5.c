86ef716e92fde8fe15eb9bc036209ecccf1dac2c
yhb <3157470765@qq.com>
2023-09-23 11:10:59 +0800

    k

--- charpter16/coding5.c
+++ charpter16/coding5.c
@@ -1,6 +1,21 @@
 #include "a.h"
 
+double wind_chill( double temp, double velocity )
+{
+    double A, B, C, X, dt;
+
+    A = 10.45;
+    B = 10.;
+    C = -1;
+    X = 1.78816;
+    dt = 33-temp;
+
+    return dt * ( A + B * sqrt( velocity ) + C * velocity ) /
+        ( A + B * sqrt( X ) + C * X );
+}
+
 void a5( void )
 {
-
+    printf( "temp = %lf, velocity = %lf\n", -5., 10. );
+    printf( "WindChill = %lf\n", wind_chill( -5., 10. ) );
 }
